20e14e789bc8aafec77a38cd53bc167f54d380ce
hannahhoward <hannah@hannahhoward.net>
2025-05-14 19:32:49 -0400

    ci(github): fix scripts to use cloudflare env

--- deploy/shared/main.tf
+++ deploy/shared/main.tf
@@ -33,7 +33,7 @@ provider "aws" {
 }
 
 module "shared" {
-  source = "github.com/storacha/storoku//shared?ref=v0.2.21"
+  source = "github.com/storacha/storoku//shared?ref=v0.2.24"
   create_db = false
   caches = [  ]
   app = var.app
