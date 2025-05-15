4e9b74024a3f453c5f98506d74a54a47addf280d
Vicente Olmedo <vicente@storacha.network>
2025-05-15 18:09:03 +0200

    storoku add tables

--- deploy/shared/main.tf
+++ deploy/shared/main.tf
@@ -33,7 +33,7 @@ provider "aws" {
 }
 
 module "shared" {
-  source = "github.com/storacha/storoku//shared?ref=v0.2.26"
+  source = "github.com/storacha/storoku//shared?ref=v0.2.27"
   create_db = false
   caches = [  ]
   app = var.app
