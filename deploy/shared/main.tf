a08b02bbab5f37f48c0df1e635e6ad17d96927bd
Vicente Olmedo <vicente@storacha.network>
2025-05-15 02:15:12 +0200

    upgrade storoku to v0.2.26

--- deploy/shared/main.tf
+++ deploy/shared/main.tf
@@ -33,7 +33,7 @@ provider "aws" {
 }
 
 module "shared" {
-  source = "github.com/storacha/storoku//shared?ref=v0.2.25"
+  source = "github.com/storacha/storoku//shared?ref=v0.2.26"
   create_db = false
   caches = [  ]
   app = var.app
