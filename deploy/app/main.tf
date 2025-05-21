4491e54b892b686a094e48834a38650a1fea7526
Vicente Olmedo <vicente@storacha.network>
2025-05-21 16:06:50 +0200

    upgrade storoku

--- deploy/app/main.tf
+++ deploy/app/main.tf
@@ -40,7 +40,7 @@ provider "aws" {
 
 
 module "app" {
-  source = "github.com/storacha/storoku//app?ref=v0.2.27"
+  source = "github.com/storacha/storoku//app?ref=v0.2.32"
   private_key = var.private_key
   private_key_env_var = "STORETHEINDEX_PRIV_KEY"
   httpport = 3000
