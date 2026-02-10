3e98c002dd4db01d5156b33f2b433f5240b259d8
Vicente Olmedo <vicente@storacha.network>
2026-02-10 12:54:33 +0100

    fix: add temporary test zone (cost-analysis)

--- deploy/shared/main.tf
+++ deploy/shared/main.tf
@@ -56,7 +56,7 @@ module "shared" {
   }
   create_db = false
   caches = []
-  networks = ["warm","forge",]
+  networks = ["warm","forge","test"]
   app = var.app
   create_shared_dev_resources = var.create_shared_dev_resources
   zone_id = var.cloudflare_zone_id
